70f4bed5da10aaa49098a98694b34df38acb049c
ulfhiraa <studyiraa@gmail.com>
2023-05-31 19:14:17 +0700

    memanggil seseorang dengan konstruktor orang, memanggil joko dengan a, memanggil lia dengan b

--- Polymorphims/Polymorphims.cpp
+++ Polymorphims/Polymorphims.cpp
@@ -28,3 +28,18 @@ public:
 		cout << "Pesan dari Lia" << endl;
 	}
 };
+
+int main() {
+	seseorang* orang;
+	joko a;
+	lia b;
+
+	orang = &a;
+	orang->pesan();
+	orang = &b;
+	orang->pesan();
+	//a.seseorang::pesan(); //memanggil method yang berada di class orang tua
+	//b.seseorang::pesan();
+
+	return 0;
+}
